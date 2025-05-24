c29a8c6970c63eae8a4cf04513f848d60ff5a3a7
naven1309 <navendrannaidoo1309@gmail.com>
2025-05-24 19:26:30 +0200

    renamed pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,29 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import Header from "./components/Header"
-import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage" // Rename this import
 import DashboardPage from "./pages/DashBoardPage"
 import EditorPage from "./pages/EditorPage"
 import MarketplacePage from "./pages/MarketplacePage"
 import MyPromptsPage from "./pages/MyPromptsPage"
 import CommunityPage from "./pages/CommunityPage"
-import FeaturesPage from "./pages/FeaturesPage"
+import HomePage from "./pages/HomePage" // Add this new import
 import ProfileSettingsPage from "./pages/ProfileSettingsPage"
 
 function App() {
+  const location = useLocation()
+  const hideHeaderRoutes = ['/'] // Add other routes where you want to hide header
+
   return (
     <>
-      <Header />
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/editor" element={<EditorPage />} />
         <Route path="/marketplace" element={<MarketplacePage />} />
         <Route path="/my-prompts" element={<MyPromptsPage />} />
         <Route path="/community" element={<CommunityPage />} />
-        <Route path="/features" element={<FeaturesPage />} />
+        <Route path="/home" element={<HomePage />} /> {/* Change route path */}
         <Route path="/profile-settings" element={<ProfileSettingsPage />} />
       </Routes>
     </>
