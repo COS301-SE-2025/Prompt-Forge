5bb4ab3cd1f65eda7ee941c21b6f5590ac1bf87f
RethabileBore <u23772141@tuks.co.za>
2025-05-25 20:36:33 +0200

    user profile

---
+++ backend/demo/src/main/java/com/example/promptforge/user_profile/dto/UpdateProfileDto.java
@@ -0,0 +1,9 @@
+package com.example.promptforge.user_profile.dto;
+
+public class UpdateProfileDto {
+    private String username;
+    private String email;
+    private String profilePicture;
+    private String password;
+    private String bio;
+}
