6fb5b4ff8176241720ae9176030ee3850ae34c0a
boitumelo.mtsatse <boitymtsatse@gmail.com>
2025-05-26 20:36:18 +0200

    clean up of user profile implementation

--- backend/demo/src/main/java/com/example/promptforge/user_profile/dto/UpdateProfileDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.promptforge.user_profile.dto;
-
-public class UpdateProfileDto {
-    private String username;
-    private String email;
-    private String profilePicture;
-    private String password;
-    private String bio;
-}
