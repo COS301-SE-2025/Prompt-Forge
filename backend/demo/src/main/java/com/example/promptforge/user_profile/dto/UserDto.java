aced664d62bc08de434ff648194803d7b687a157
boitumelo.mtsatse <boitymtsatse@gmail.com>
2025-05-26 20:36:18 +0200

    clean up of user profile implementation

--- backend/demo/src/main/java/com/example/promptforge/user_profile/dto/UserDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.promptforge.user_profile.dto;
-
-public class UserDto {
-    private String username;
-    private String email;
-    private String profilePicture;
-    private String bio;
-}
--- backend/demo/src/main/java/com/example/promptforge/user_profile/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.promptforge.user_profile.repository;
-
-import com.example.promptforge.user_profile.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
