89e0bd355e24732a5f4e84306337a715e85d5aa2
u22658395 <u22658395@tuks.co.za>
2025-05-27 13:39:50 +0200

    Created the ReecentActivity component used to display recent activity

---
+++ frontend/src/components/RecentActivity.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+interface RecentActivityProps{
+  username:string,
+  activity:string,
+  time:string
+}
+export function RecentActivity({username, activity, time}:RecentActivityProps) {
+  return <div className="mb-2 flex text-muted-foreground justify-between items-center w-full">
+    <div className="flex items-center">
+        <span className="text-[#0066e2] pr-1">{username} <span className="text-muted-foreground">{activity}</span></span>
+    </div>
+    <div>{time} ago</div>
+    </div>;
+}
+  
