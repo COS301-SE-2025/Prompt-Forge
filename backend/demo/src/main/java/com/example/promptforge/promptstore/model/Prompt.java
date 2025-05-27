4e0b4b1456cbc87a817255d7becc091c3e8965df
boitumelo.mtsatse <boitymtsatse@gmail.com>
2025-05-27 16:08:13 +0200

    jsonIgore- to only show prompt and its metadata once, avoiding circular dependencies;; changing mapping as well

--- backend/demo/src/main/java/com/example/promptforge/promptstore/model/Prompt.java
+++ backend/demo/src/main/java/com/example/promptforge/promptstore/model/Prompt.java
@@ -7,6 +7,8 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -70,5 +72,6 @@ public class Prompt {
     cascade = CascadeType.ALL,
     fetch = FetchType.LAZY
 )
+@JsonIgnore
 private PromptMetadata metadata;
 }
