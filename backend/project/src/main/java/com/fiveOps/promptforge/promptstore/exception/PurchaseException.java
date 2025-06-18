c528303d1b834c6140796e7e907d2d685bf6df01
boitumelo.mtsatse <boitymtsatse@gmail.com>
2025-06-18 15:29:53 +0200

    exception class

---
+++ backend/project/src/main/java/com/fiveOps/promptforge/promptstore/exception/PurchaseException.java
@@ -0,0 +1,7 @@
+package com.fiveOps.promptforge.promptstore.exception;
+
+public class PurchaseException extends RuntimeException {
+    public PurchaseException(String message) {
+        super(message);
+    }
+}
