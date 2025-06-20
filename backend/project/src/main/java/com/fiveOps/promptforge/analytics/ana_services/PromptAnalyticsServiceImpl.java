fc8108bb77e10145ce05d00b37d1cfac8428a14f
PaballoDiyase888 <diyasepaballo@gmail.com>
2025-06-20 04:09:11 +0200

    analtics dtos & services

--- backend/project/src/main/java/com/fiveOps/promptforge/analytics/ana_services/PromptAnalyticsServiceImpl.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/analytics/ana_services/PromptAnalyticsServiceImpl.java
@@ -43,7 +43,7 @@
 //     }
 // }
 
-package com.fiveOps.promptforge.analytics.ana_services;
+package main.java.com.fiveOps.promptforge.analytics.ana_services;
 
 import com.fiveOps.promptforge.analytics.ana_dto.*;
 import com.fiveOps.promptforge.analytics.ana_repository.PromptAnalyticsRepository;
