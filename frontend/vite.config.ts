672c772aa0f5d34af964bb00c8fd7959b14d86e6
PaballoDiyase888 <diyasepaballo@gmail.com>
2025-06-21 19:19:43 +0200

    dash integration complete to date

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,24 @@
+// import { defineConfig } from "vite"
+// import react from "@vitejs/plugin-react"
+// import path from "path"
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   server: {
+//     port: 5173,
+//   },
+// })
+
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -12,5 +28,8 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    proxy: {
+      '/api': 'http://localhost:8080', // Only used in dev
+    },
   },
 })
