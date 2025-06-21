dd3058b6f5b47b246518c4b328cd114576f9d303
u22658395 <u22658395@tuks.co.za>
2025-06-22 01:35:18 +0200

    Query to get all the marketplace prompts and their respective tags and author username

--- backend/project/src/main/java/com/fiveOps/promptforge/promptstore/repository/PromptStoreRepository.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/promptstore/repository/PromptStoreRepository.java
@@ -2,19 +2,42 @@
 package com.fiveOps.promptforge.promptstore.repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fiveOps.promptforge.prompts.model.Prompt;
+import com.fiveOps.promptforge.prompts.model.PromptWithAuthorDTO;
 
 @Repository
 public interface PromptStoreRepository extends JpaRepository<Prompt, UUID> {
     List<Prompt> findByVisibility(String visibility);
-    
+    @Query(value = """
+       SELECT
+              p.prompt_id AS id,
+              p.author_id AS authorId,
+              p.title AS title,
+              p.slug AS slug,
+              p.description AS description,
+              p.price AS price,
+              u.username AS username,
+              array_agg(t.name) AS tagNames
+       FROM
+              prompts p
+       JOIN users u ON p.author_id = u.user_id
+       LEFT JOIN tags t ON t.tag_id = ANY(p.prompt_tags)
+       WHERE p.visibility = 'public'
+       GROUP BY p.prompt_id, u.username
+       LIMIT :size OFFSET :offset
+       """, nativeQuery = true)
+    List<Map<String, PromptWithAuthorDTO>> findPublicPromptsWithAuthorAndTags(@Param("size") int size, @Param("offset") int offset);
+
     @Query("SELECT p FROM Prompt p WHERE " +
            "LOWER(p.title) LIKE LOWER(CONCAT('%', :query, '%')) " +
            "AND p.visibility = 'public'")
