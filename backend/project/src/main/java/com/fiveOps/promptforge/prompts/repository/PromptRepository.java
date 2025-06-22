7080fcbb4c654fc2660d911260a706655671071c
boitumelo.mtsatse <boitymtsatse@gmail.com>
2025-06-23 00:47:46 +0200

    paginated prompt repo

--- backend/project/src/main/java/com/fiveOps/promptforge/prompts/repository/PromptRepository.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/prompts/repository/PromptRepository.java
@@ -3,6 +3,8 @@ package com.fiveOps.promptforge.prompts.repository;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,22 +16,23 @@ import com.fiveOps.promptforge.prompts.model.Prompt;
 public interface PromptRepository extends JpaRepository<Prompt, UUID> {
 
      List<Prompt> findByFeaturedTrue();
+     Page<Prompt> findAll(Pageable pageable);
 
-    List<Prompt> findByVisibility(String visibility);
-    //List<Prompt> findByCategoryAndVisibility(String category, String visibility);
-    List<Prompt> findByAuthorId(UUID authorId);
-    List<Prompt> findByTitleContainingIgnoreCase(String title);
+    Page<Prompt> findByVisibility(String visibility,Pageable pageable);
+    //Page<Prompt> findByCategoryAndVisibility(String category, String visibility);
+    Page<Prompt> findByAuthorId(UUID authorId,Pageable pageable);
+    Page<Prompt> findByTitleContainingIgnoreCase(String title,Pageable pageable);
     @Query("SELECT p FROM Prompt p WHERE " +
            "LOWER(p.title) LIKE LOWER(CONCAT('%', :searchTerm, '%')) " +
            "AND p.visibility = 'public'")
-    List<Prompt> searchPublicByTitle(@Param("searchTerm") String searchTerm);
+    Page<Prompt> searchPublicByTitle(@Param("searchTerm") String searchTerm,Pageable pageable);
     
     @Query(value = "SELECT * FROM prompts WHERE :tagId = ANY(prompt_tags)", 
            nativeQuery = true)
-    List<Prompt> findByTagId(@Param("tagId") UUID tagId);
+    Page<Prompt> findByTagId(@Param("tagId") UUID tagId,Pageable pageable);
 
     @Query(value = "SELECT * FROM prompts WHERE visibility = 'public' AND price <= :maxPrice", nativeQuery = true)
-    List<Prompt> findPublicPromptsUnderPrice(@Param("maxPrice") double maxPrice);
+    Page<Prompt> findPublicPromptsUnderPrice(@Param("maxPrice") double maxPrice,Pageable pageable);
 
     
 
