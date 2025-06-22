15294e2476e417c1dc93ba06a3c701ef5cb33f04
boitumelo.mtsatse <boitymtsatse@gmail.com>
2025-06-23 00:46:59 +0200

    updated PromptForgeApplication with caching enabled

--- backend/project/src/main/java/com/fiveOps/promptforge/PromptForgeApplication.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/PromptForgeApplication.java
@@ -3,8 +3,10 @@ package com.fiveOps.promptforge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PromptForgeApplication {
 	public static void main(String[] args) {
 
