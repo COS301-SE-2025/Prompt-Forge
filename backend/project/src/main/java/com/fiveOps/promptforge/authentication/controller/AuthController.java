11202e80875f4fbb3f89ee61c57b28f57ae1bf3e
RethabileBore <u23772141@tuks.co.za>
2025-06-27 00:32:40 +0200

    Removed GoogleVerification

--- backend/project/src/main/java/com/fiveOps/promptforge/authentication/controller/AuthController.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/authentication/controller/AuthController.java
@@ -1,31 +1,19 @@
 package com.fiveOps.promptforge.authentication.controller;   
 
-import java.time.LocalDateTime;
-import java.util.UUID;
+
 import java.util.Map;
 import java.util.HashMap;
-
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-
-import com.fiveOps.promptforge.authentication.dto.AuthResponse;
 import com.fiveOps.promptforge.authentication.dto.LoginRequest;
-import com.fiveOps.promptforge.authentication.dto.GoogleLoginRequest;
 import com.fiveOps.promptforge.authentication.dto.SignupRequest;
 import com.fiveOps.promptforge.authentication.service.AuthService;
 import com.fiveOps.promptforge.user_profile.model.User;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import jakarta.servlet.http.HttpServletResponse;
-
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
@@ -94,13 +82,6 @@ public class AuthController {
         }
     }
     
-
-
-    @PostMapping("/google")
-    public ResponseEntity<AuthResponse> loginWithGoogle(@RequestBody GoogleLoginRequest request) {
-        AuthResponse authResponse = authService.loginWithGoogle(request);
-        return ResponseEntity.ok(authResponse);
-    }
 
     @PostMapping("/logout")
 public ResponseEntity<?> logout(HttpServletResponse response) {
