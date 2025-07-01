efabaa0622ffc30edbb08a689b3a663e7a7a46ef
u22658395 <u22658395@tuks.co.za>
2025-07-01 16:52:51 +0200

    Finder Method to get purchased prompts

--- backend/project/src/main/java/com/fiveOps/promptforge/prompts/repository/PromptRepository.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/prompts/repository/PromptRepository.java
@@ -1,14 +1,18 @@
 package com.fiveOps.promptforge.prompts.repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fiveOps.promptforge.prompts.model.Prompt;
+import com.fiveOps.promptforge.prompts.model.PromptWithAuthorDTO;
 
 @Repository
 public interface PromptRepository extends JpaRepository<Prompt, UUID> {
@@ -30,7 +34,44 @@ public interface PromptRepository extends JpaRepository<Prompt, UUID> {
 
     @Query(value = "SELECT * FROM prompts WHERE visibility = 'public' AND price <= :maxPrice", nativeQuery = true)
     List<Prompt> findPublicPromptsUnderPrice(@Param("maxPrice") double maxPrice);
-
+    
+   @Query(value = """
+       SELECT
+              p.prompt_id AS id,
+              p.author_id AS authorId,
+              p.title AS title,
+              p.slug AS slug,
+              p.description AS description,
+              p.price AS price,
+              author_user.username AS username,
+              array_agg(t.name) AS tagNames
+       FROM
+              purchased_prompts pp
+       JOIN prompts p ON pp.prompt_id = p.prompt_id
+       JOIN users author_user ON p.author_id = author_user.user_id
+       LEFT JOIN tags t ON t.tag_id = ANY(p.prompt_tags)
+       WHERE pp.user_id = :user_id
+       GROUP BY p.prompt_id, author_user.username, p.author_id, p.title, p.slug,p.description, p.price
+       """, 
+       countQuery = """
+       SELECT
+              p.prompt_id AS id,
+              p.author_id AS authorId,
+              p.title AS title,
+              p.slug AS slug,
+              p.description AS description,
+              p.price AS price,
+              author_user.username AS username,
+              array_agg(t.name) AS tagNames
+       FROM
+              purchased_prompts pp
+       JOIN prompts p ON pp.prompt_id = p.prompt_id
+       JOIN users author_user ON p.author_id = author_user.user_id
+       LEFT JOIN tags t ON t.tag_id = ANY(p.prompt_tags)
+       WHERE pp.user_id = :user_id
+       """,
+       nativeQuery = true)
+       Page<Map<String, PromptWithAuthorDTO>> getPurchasedPromptsByUserId(@Param("user_id") UUID userId, Pageable pageable);
     
 
     //long countByCategory(String category);
