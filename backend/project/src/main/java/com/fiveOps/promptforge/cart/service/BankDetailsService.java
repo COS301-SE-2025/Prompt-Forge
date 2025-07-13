8f7bd33165587fbe399f61816fba0efddea5bc1c
u22658395 <u22658395@tuks.co.za>
2025-07-13 22:13:28 +0200

    BankDetails service with method to get a user's subaccount ID

---
+++ backend/project/src/main/java/com/fiveOps/promptforge/cart/service/BankDetailsService.java
@@ -0,0 +1,21 @@
+package com.fiveOps.promptforge.cart.service;
+
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+import com.fiveOps.promptforge.cart.repository.BankDetailsRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BankDetailsService {
+
+  private final BankDetailsRepository bankDetailsRepository;
+  
+  public String getSubaccountIDByUserID(UUID userId) {
+    String userDetails = bankDetailsRepository.findByUserId(userId)
+      .orElseThrow(()-> new RuntimeException("Recipient code not found for user"));
+    return userDetails;
+  }
+
+  
+}
