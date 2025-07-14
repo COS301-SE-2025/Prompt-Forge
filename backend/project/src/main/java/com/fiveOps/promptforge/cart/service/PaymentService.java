560e1dc1f4c7a16de26535f7eb197eb3294a4987
u22658395 <u22658395@tuks.co.za>
2025-07-14 21:23:50 +0200

    Method to initialize split payment

--- backend/project/src/main/java/com/fiveOps/promptforge/cart/service/PaymentService.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/cart/service/PaymentService.java
@@ -1,6 +1,7 @@
 package com.fiveOps.promptforge.cart.service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -11,6 +12,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -53,4 +57,49 @@ public class PaymentService {
 
   }
 
+  public String initializeSplitPayment(String customerEmail, List<Map<String, 
+    Object>> subaccounts, int total) {
+    String url = gatewayURL+"transaction/initialize";
+
+    // Set headers
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.set("Authorization", "Bearer " + paystackSecretKey);
+
+    // Create split object
+    Map<String, Object> split = new HashMap<>();
+    split.put("type", "flat");
+    split.put("bearer_type", "account");
+    split.put("subaccounts", subaccounts);
+
+    //request body
+    Map<String, Object> body = new HashMap<>();
+    body.put("email", customerEmail);
+    body.put("amount", total);
+    body.put("split", split);
+
+    HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
+
+    try {
+      ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+      System.out.println("Paystack response:");
+      System.out.println(response.getBody());
+
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode responseBody = mapper.readTree(response.getBody());
+
+      Boolean status = responseBody.path("status").asBoolean();
+      if (status) {
+        String accessCode = responseBody.path("data").path("access_code").asText();
+        return accessCode;
+      }
+
+      throw new RuntimeException();
+      // Optional: parse and extract authorization_url if needed
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException("Error checking out");
+      // Optionally rethrow or wrap exception
+    }
+  }
 }
