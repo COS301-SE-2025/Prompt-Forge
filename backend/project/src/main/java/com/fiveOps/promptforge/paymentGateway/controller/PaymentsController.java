a0493e3d21885b34414b6f019d59f0e80adadb94
u22658395 <u22658395@tuks.co.za>
2025-07-15 20:24:06 +0200

    payments controller

---
+++ backend/project/src/main/java/com/fiveOps/promptforge/paymentGateway/controller/PaymentsController.java
@@ -0,0 +1,45 @@
+package com.fiveOps.promptforge.paymentGateway.controller;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.fiveOps.promptforge.cart.dto.CartItemDTO;
+import com.fiveOps.promptforge.cart.dto.APIResponse;
+import com.fiveOps.promptforge.paymentGateway.dto.InitializePaymentRequest;
+import com.fiveOps.promptforge.paymentGateway.dto.TransactionInitializationResponse;
+import com.fiveOps.promptforge.paymentGateway.service.PaymentService;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentsController {
+
+  private final PaymentService paymentService;
+
+  public PaymentsController(PaymentService paymentService) {
+    this.paymentService = paymentService;
+  }
+
+  @PostMapping("/initialize")
+  public ResponseEntity<APIResponse> checkoutCart(
+      @RequestBody InitializePaymentRequest request, Authentication authentication) {
+    try {
+      String userEmail = authentication.getName();
+      List<CartItemDTO> prompts = request.getPrompts();
+
+      TransactionInitializationResponse transactionAccessCodeAndReference = paymentService.initializePayment(userEmail,
+          prompts, request.getTotal());
+
+      return ResponseEntity.ok(new APIResponse("success", "Transaction initialization successful",
+          transactionAccessCodeAndReference));
+    } catch (Exception e) {
+      System.err.println("Checkout error: " + e.getMessage());
+      e.printStackTrace();
+      return ResponseEntity.badRequest()
+          .body(new APIResponse("error", "Checkout failed: " + e.getMessage()));
+    }
+  }
+}
