103d05da47070b52ff23741427c4d9863c20a6d0
u22658395 <u22658395@tuks.co.za>
2025-07-15 20:30:10 +0200

    removed the redundant method passed as prop to CartSummary. Calls to checkout and initializePayment. Paystack popup

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -114,7 +114,6 @@ export default function CartPage() {
                         <CartSummary 
                             subtotal={subtotal} 
                             prompts={cartItems} 
-                            setCartItems={setCartItems}
                             onCheckoutSuccess={handleCheckoutSuccess}
                         />
                     )}
