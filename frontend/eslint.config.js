32e34d7b5dabf56f6a16fc5be198773631356294
PaballoDiyase888 <diyasepaballo@gmail.com>
2025-07-15 17:28:50 +0200

    eslint fixes

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,31 +2,20 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import { FlatCompat } from '@eslint/eslintrc';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
 
-const compat = new FlatCompat();
-
-export default [
+export default tseslint.config(
   { ignores: ['dist'] },
-  js.configs.recommended,
-  ...compat.config({
-    extends: [
-      'plugin:@typescript-eslint/recommended'
-    ],
-  }),
   {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: tsParser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      '@typescript-eslint': tseslint,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -38,4 +27,4 @@ export default [
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
-];
+);
