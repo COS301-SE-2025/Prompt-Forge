91e12c0cc86c6e65ba3e9d31e83f2283f5439cd4
u22658395 <u22658395@tuks.co.za>
2025-07-17 04:15:22 +0200

    error handling for non-existent Author bank details

--- backend/project/src/main/java/com/fiveOps/promptforge/paymentGateway/service/BankDetailsService.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/paymentGateway/service/BankDetailsService.java
@@ -12,8 +12,9 @@ public class BankDetailsService {
   private final BankDetailsRepository bankDetailsRepository;
   
   public String getSubaccountIDByUserID(UUID userId) {
+    System.out.println("userId:"+ userId);
     String userDetails = bankDetailsRepository.findByUserId(userId)
-      .orElseThrow(()-> new RuntimeException("Recipient code not found for user"));
+      .orElseThrow(()-> new RuntimeException("Author payment details not found"));
     return userDetails;
   }
 
