916706613d6c58f947c897c20f18cb2f30c7fd33
u22658395 <u22658395@tuks.co.za>
2025-08-01 16:57:48 +0200

    Payout card DTO

---
+++ backend/project/src/main/java/com/fiveOps/promptforge/payments/dto/PayoutCardDTO.java
@@ -0,0 +1,36 @@
+package com.fiveOps.promptforge.payments.dto;
+
+public class PayoutCardDTO {
+  private BankDTO bank;
+  private String accountNumber;
+  private String cardHolderName;
+
+  public String getBankCode() {
+    return bank.getCode();
+  }
+  
+  public String getBankName() {
+    return bank.getName();
+  }
+
+  public String getAccountNumber() {
+    return accountNumber;
+  }
+
+  public String getCardHolderName() {
+    return cardHolderName;
+  }
+
+  public void setBank(BankDTO bank) {
+    this.bank = bank;
+  }
+
+  public void setAccountNumber(String accountNumber) {
+    this.accountNumber = accountNumber;
+  }
+
+  public void setCardHolderName(String cardHolderName) {
+    this.cardHolderName = cardHolderName;
+  }
+
+}
