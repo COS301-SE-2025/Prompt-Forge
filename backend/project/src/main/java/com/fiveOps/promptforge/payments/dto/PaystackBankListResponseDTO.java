a70d7e8605bc9c15c09a4dd4f847fee5a465647d
u22658395 <u22658395@tuks.co.za>
2025-08-01 17:04:23 +0200

    method to add payout details to db

--- backend/project/src/main/java/com/fiveOps/promptforge/payments/dto/PaystackBankListResponseDTO.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/payments/dto/PaystackBankListResponseDTO.java
@@ -3,37 +3,37 @@ package com.fiveOps.promptforge.payments.dto;
 import java.util.List;
 
 public class PaystackBankListResponseDTO {
-    private Boolean status;
-    private String message;
-    private List<BankDTO> data;
+  private Boolean status;
+  private String message;
+  private List<BankDTO> data;
 
-    public PaystackBankListResponseDTO(Boolean status, String message, List<BankDTO> data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
+  public PaystackBankListResponseDTO(Boolean status, String message, List<BankDTO> data) {
+    this.status = status;
+    this.message = message;
+    this.data = data;
+  }
 
-    public Boolean getStatus() {
-        return status;
-    }
+  public Boolean getStatus() {
+    return status;
+  }
 
-    public String getMessage() {
-        return message;
-    }
+  public String getMessage() {
+    return message;
+  }
 
-    public List<BankDTO> getData() {
-        return data;
-    }
+  public List<BankDTO> getData() {
+    return data;
+  }
 
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
+  public void setStatus(Boolean status) {
+    this.status = status;
+  }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+  public void setMessage(String message) {
+    this.message = message;
+  }
 
-    public void setData(List<BankDTO> data) {
-        this.data = data;
-    }
+  public void setData(List<BankDTO> data) {
+    this.data = data;
+  }
 }
