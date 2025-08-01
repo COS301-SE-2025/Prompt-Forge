a385877dc890be6acee6f3a2ad607143618d2622
u22658395 <u22658395@tuks.co.za>
2025-08-01 23:28:03 +0200

    changed query to use dto instead of projection

--- backend/project/src/main/java/com/fiveOps/promptforge/payments/repository/BankDetailsRepository.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/payments/repository/BankDetailsRepository.java
@@ -7,13 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.fiveOps.promptforge.payments.dto.PayoutCardWithSubaccountCodeDTO;
 import com.fiveOps.promptforge.payments.model.BankAccount;
-import com.fiveOps.promptforge.payments.projection.PaymentDetailsProjectionWithPaystackSubaccountCode;
 
 @Repository
 public interface BankDetailsRepository extends JpaRepository<BankAccount, UUID> {
 
-  PaymentDetailsProjectionWithPaystackSubaccountCode findByUserUserId(
+  PayoutCardWithSubaccountCodeDTO findByUserUserId(
       @Param("user_id") UUID userId);
 
   Integer countByUserUserId(@Param("user_id") UUID userId);
