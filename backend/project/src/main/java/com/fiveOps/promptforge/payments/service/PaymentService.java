442ed23abb95f1455bfafe4fcbbae47fcc0b4016
Katlego Mositi <126470555+KatlegoMositi@users.noreply.github.com>
2025-08-02 17:52:09 +0200

    Revert "Payment details"

--- backend/project/src/main/java/com/fiveOps/promptforge/payments/service/PaymentService.java
+++ backend/project/src/main/java/com/fiveOps/promptforge/payments/service/PaymentService.java
@@ -7,27 +7,16 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fiveOps.promptforge.cart.dto.CartItemDTO;
-import com.fiveOps.promptforge.payments.dto.BankDTO;
-import com.fiveOps.promptforge.payments.dto.PayoutCardDTO;
-import com.fiveOps.promptforge.payments.dto.PaystackAddSubaccountResponseDTO;
-import com.fiveOps.promptforge.payments.dto.PaystackBankListResponseDTO;
-import com.fiveOps.promptforge.payments.dto.PaystackErrorResponseDTO;
-import com.fiveOps.promptforge.payments.dto.PaystackResponseDTO;
 import com.fiveOps.promptforge.payments.dto.TransactionInitializationResponse;
 import com.fiveOps.promptforge.user_profile.service.UserService;
 
@@ -40,7 +29,6 @@ public class PaymentService {
   private final RestTemplate restTemplate;
   private final UserService userService;
   private final BankDetailsService bankDetailsService;
-  private final ObjectMapper objectMapper;
   private final String gatewayURL = "https://api.paystack.co/";
 
   @Value("${paystack.secret-key}")
@@ -164,7 +152,8 @@ public class PaymentService {
       if (authorShares.size() == 1) {
         Map.Entry<UUID, Integer> authorShareEntry = authorShares.entrySet().iterator().next();
         UUID authorId = authorShareEntry.getKey();
-        String subaccountCode = bankDetailsService.getSubaccountCodeByUserID(authorId);
+        // Integer authorShare = authorShareEntry.getValue();
+        String subaccountCode = bankDetailsService.getSubaccountIDByUserID(authorId);
         return inititalizeSingleAuthorPayment(
             customerEmail, subaccountCode, authorId, roundedTotalInCents);
       } else {
@@ -176,7 +165,7 @@ public class PaymentService {
           UUID authorId = entry.getKey();
           Integer authorShare = entry.getValue();
           totalCalculated += authorShare;
-          String subaccountCode = bankDetailsService.getSubaccountCodeByUserID(authorId);
+          String subaccountCode = bankDetailsService.getSubaccountIDByUserID(authorId);
           Map<String, Object> sub = new HashMap<>();
           sub.put("subaccount", subaccountCode);
           sub.put("share", authorShare); // share in kobo
@@ -188,190 +177,6 @@ public class PaymentService {
     } catch (Exception e) {
       System.out.println("error purchasing:");
       System.out.println(e);
-      throw e;
-    }
-  }
-
-  public List<BankDTO> getBankList() {
-
-    String secretKey = "Bearer " + paystackSecretKey;
-
-    // headers
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    headers.set("Authorization", secretKey);
-
-    // Make the GET request
-    try {
-      ResponseEntity<PaystackBankListResponseDTO> responseEntity =
-          restTemplate.getForEntity(
-              gatewayURL + "bank?country=south africa", PaystackBankListResponseDTO.class);
-      PaystackBankListResponseDTO response = responseEntity.getBody();
-
-      if (response.getStatus()) {
-        return response.getData();
-      }
-
-      throw new RuntimeException();
-    } catch (Exception e) {
-      // TODO: handle exception
-      e.printStackTrace();
-      throw new RuntimeException();
-    }
-  }
-
-  public PaystackAddSubaccountResponseDTO addSubaccount(String username, PayoutCardDTO payoutCard)
-      throws Exception {
-    String secretKey = "Bearer " + paystackSecretKey;
-
-    // headers
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    headers.set("Authorization", secretKey);
-
-    // Body
-    Map<String, Object> body = new HashMap<>();
-    body.put("business_name", username);
-    body.put("bank_code", payoutCard.getBank().getCode());
-    body.put("account_number", payoutCard.getAccountNumber());
-    body.put("percentage_charge", 0);
-
-    HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
-
-    // Make the POST request
-    try {
-      ResponseEntity<PaystackResponseDTO<PaystackAddSubaccountResponseDTO>> responseEntity =
-          restTemplate.exchange(
-              gatewayURL + "subaccount",
-              HttpMethod.POST,
-              request, // request body + headers
-              new ParameterizedTypeReference<
-                  PaystackResponseDTO<PaystackAddSubaccountResponseDTO>>() {});
-
-      // Output response
-      System.out.println("\n\nStatus Code: " + responseEntity.getStatusCode());
-      System.out.println("Response Body:\n" + responseEntity.getBody() + "");
-
-      System.out.println("before response");
-      PaystackResponseDTO<PaystackAddSubaccountResponseDTO> response = responseEntity.getBody();
-      System.out.println("after response");
-
-      System.out.println("dataaaa:" + response.getData());
-
-      if (responseEntity.getStatusCode() == HttpStatusCode.valueOf(201)) {
-        if (response.getStatus()) {
-          System.out.println("get status is trueeeeeee");
-          PaystackAddSubaccountResponseDTO data = response.getData();
-
-          System.out.println("subaccoutCode:" + data.getSubaccountCode());
-          System.out.println("isVerified:" + data.getVerification());
-          return data;
-        }
-      } else {
-        if (responseEntity.getStatusCode() == HttpStatusCode.valueOf(400)) {
-          System.out.println("\n\n hereeeeeeeeeee throwing response.getmessage");
-          throw new RuntimeException(response.getMessage());
-        }
-      }
-      System.out.println("status:" + response.getMessage());
-      throw new RuntimeException("Unable to create subaccount");
-    } catch (HttpClientErrorException e) {
-      if (e.getStatusCode().value() == 400) {
-        try {
-          PaystackErrorResponseDTO errorResponse =
-              objectMapper.readValue(e.getResponseBodyAsString(), PaystackErrorResponseDTO.class);
-          System.err.println("Validation failed: " + errorResponse.getMessage());
-          throw new RuntimeException(errorResponse.getMessage());
-        } catch (JsonMappingException runEx) {
-          throw new RuntimeException("Internal server error");
-        } catch (Exception runEx) {
-          throw runEx;
-        }
-      } else {
-        System.err.println("Unhandled error: " + e.getMessage());
-        throw new RuntimeException(e.getMessage());
-      }
-    } catch (Exception e) {
-      System.out.println("last exception:" + e.getMessage());
-      throw e;
-    }
-  }
-
-  public void updateSubaccount(String subaccountCode, String username, PayoutCardDTO payoutCard)
-      throws Exception {
-    String secretKey = "Bearer " + paystackSecretKey;
-
-    // headers
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    headers.set("Authorization", secretKey);
-
-    // Body
-    Map<String, Object> body = new HashMap<>();
-    body.put("business_name", username);
-    body.put("bank_code", payoutCard.getBank().getCode());
-    body.put("account_number", payoutCard.getAccountNumber());
-    body.put("percentage_charge", 0);
-
-    HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
-
-    // Make the PUT request
-    try {
-      ResponseEntity<PaystackResponseDTO<PaystackAddSubaccountResponseDTO>> responseEntity =
-          restTemplate.exchange(
-              gatewayURL + "subaccount/" + subaccountCode,
-              HttpMethod.PUT,
-              request,
-              new ParameterizedTypeReference<
-                  PaystackResponseDTO<PaystackAddSubaccountResponseDTO>>() {});
-
-      // Output response
-      System.out.println("\n\nStatus Code: " + responseEntity.getStatusCode());
-      System.out.println("Response Body:\n" + responseEntity.getBody() + "");
-
-      System.out.println("before response");
-      PaystackResponseDTO<PaystackAddSubaccountResponseDTO> response = responseEntity.getBody();
-      System.out.println("after response");
-
-      System.out.println("dataaaa:" + response.getData());
-
-      if (responseEntity.getStatusCode() == HttpStatusCode.valueOf(200)) {
-        if (response.getStatus()) {
-          System.out.println("get status is trueeeeeee");
-          PaystackAddSubaccountResponseDTO data = response.getData();
-
-          System.out.println("subaccoutCode:" + data.getSubaccountCode());
-          System.out.println("isVerified:" + data.getVerification());
-          return;
-        }
-      } else {
-        if (responseEntity.getStatusCode() == HttpStatusCode.valueOf(400)) {
-          System.out.println("\n\n hereeeeeeeeeee throwing response.getmessage");
-          throw new RuntimeException(response.getMessage());
-        }
-      }
-      System.out.println("status:" + response.getMessage());
-      throw new RuntimeException("Unable to update payout details");
-    } catch (HttpClientErrorException e) {
-      if (e.getStatusCode().value() == 400) {
-        try {
-          PaystackErrorResponseDTO errorResponse =
-              objectMapper.readValue(e.getResponseBodyAsString(), PaystackErrorResponseDTO.class);
-
-          System.err.println("Validation failed: " + errorResponse.getMessage());
-          throw new RuntimeException(errorResponse.getMessage());
-
-        } catch (JsonMappingException runEx) {
-          throw new RuntimeException("Internal server error");
-        } catch (Exception runEx) {
-          throw runEx;
-        }
-      } else {
-        System.err.println("Unhandled error: " + e.getMessage());
-        throw new RuntimeException(e.getMessage());
-      }
-    } catch (Exception e) {
-      System.out.println("last exception:" + e.getMessage());
       throw e;
     }
   }
