532a769706a1abc1b14fbc5c91f4f9b1a16abab7
naven1309 <navendrannaidoo1309@gmail.com>
2025-08-06 12:17:37 +0200

    Added api gateway to get around multi content errors

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 
-  'https://ec2-13-51-199-205.eu-north-1.compute.amazonaws.com:8080/api';
+  'https://69v54mpz44.execute-api.eu-north-1.amazonaws.com/prod/api';
 
 export { API_BASE_URL };
 export default API_BASE_URL;
