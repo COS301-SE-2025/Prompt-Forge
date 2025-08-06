7af79f99437abd28657d200650691a6681d78ac1
naven1309 <navendrannaidoo1309@gmail.com>
2025-08-06 11:41:12 +0200

    base url changed

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 
-  '/api';
+  'https://ec2-13-51-199-205.eu-north-1.compute.amazonaws.com:8080/api';
 
 export { API_BASE_URL };
 export default API_BASE_URL;
