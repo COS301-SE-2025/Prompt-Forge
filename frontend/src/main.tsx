41f5f6fd511ec976d73a31689cb31fe3a9f647d9
naven1309 <navendrannaidoo1309@gmail.com>
2025-08-12 11:44:12 +0200

    sign in with google added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "./styles/index.css"
 import { ThemeProvider } from "./components/theme-provider"
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark">
-        <App />
+        <GoogleOAuthProvider clientId="1042356002829-220hqtgm6scuhhke4stc52bib8q6gk1s.apps.googleusercontent.com">
+          <App />
+        </GoogleOAuthProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
