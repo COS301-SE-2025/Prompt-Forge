bc0c3f9de15b8c71722b5b0d21089c12dd59c9ba
RethabileBore <u23772141@tuks.co.za>
2025-08-13 09:12:39 +0200

    prod-api

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 
-  'http://localhost:8080/api';
+  'https://69v54mpz44.execute-api.eu-north-1.amazonaws.com/prod/api';
 
 export { API_BASE_URL };
 export default API_BASE_URL;
