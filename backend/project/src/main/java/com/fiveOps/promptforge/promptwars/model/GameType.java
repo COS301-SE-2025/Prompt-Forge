74cea5fb381fbd192c955b0d8dde1b8658cc851a
boitumelo.mtsatse <boitymtsatse@gmail.com>
2025-09-16 15:31:05 +0200

    model for enum game types

---
+++ backend/project/src/main/java/com/fiveOps/promptforge/promptwars/model/GameType.java
@@ -0,0 +1,6 @@
+package com.fiveOps.promptforge.promptwars.model;
+
+public enum GameType {
+  PROMPT_CREATION, // Original game where players create prompts
+  REVERSE_PROMPT // New game where players guess which prompt generated an output
+}
