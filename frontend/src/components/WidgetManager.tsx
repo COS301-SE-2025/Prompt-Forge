c17afa23f2d805a6613e35c7b5474299d06cf5e3
Paballo Diyase <diyasepaballo@gmail.com>
2025-09-23 15:38:47 +0200

    engaged views

--- frontend/src/components/WidgetManager.tsx
+++ frontend/src/components/WidgetManager.tsx
@@ -334,7 +334,7 @@ export default function WidgetManager({
         
         const components = [
           { 
-            name: "Engaged Views", 
+            name: "Views", 
             value: viewsBarWidth, // Bar width shows engagement rate
             count: totalViews,
             percentage: `${engagementRate.toFixed(1)}%`, // Views that didn't bounce = engagement rate
